650fab61a2c8b8778c877b5eaf781f210dbd2381
lumi2021 <94462076+lumi2021@users.noreply.github.com>
2026-04-26 21:31:33 -0300

    add include

--- src/graphics/graphics.h
+++ src/graphics/graphics.h
@@ -6,7 +6,6 @@
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_video.h>
 #include <SDL3/SDL_events.h>
-#include <SDL3/SDL_init.h>
 #include <SDL3/SDL_oldnames.h>
 #include <SDL3/SDL_surface.h>
 #include <SDL3/SDL_video.h>
